752d7455ad21413439f7e0b6d8ea3a0529c1d8e7
mohamedamin12 <aboamin909080@gmail.com>
2025-07-25 22:00:29 +0300

    Refactor frontend and backend configurations: simplify backend build script, enhance frontend dependencies with Tailwind CSS and additional libraries, implement routing in App component, and set up proxy in Vite config. Remove unused App.css file.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext.jsx";
+import { SocketContextProvider } from "./contexts/SocketContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthContextProvider>
+        <SocketContextProvider>
+          <App />
+        </SocketContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
